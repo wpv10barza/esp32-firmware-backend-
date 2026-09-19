17e32dc1213a4e8aefa7a7d518b2d07d6d165c17
wpv10barza <polovaleriow@gmail.com>
2026-09-18 23:53:42 -0500

    test(style): lock Guition page background and surface tokens

--- test/guition_ui_style_contract.py
+++ test/guition_ui_style_contract.py
@@ -24,6 +24,9 @@ for token in [
     require(STYLE, token)
 
 require(MAIN, '#include "ui_style.h"')
+# The Guition reference uses a black page background and slate-blue-gray surfaces.
+require(STYLE, "kBackground = rgb565(0x0D, 0x0D, 0x0D)", "Guition page background")
+require(STYLE, "kSurface = rgb565(0x34, 0x36, 0x45)", "Guition surface")
 require(MAIN, "void clearScreenForMode()")
 require(MAIN, "void drawEditorText()")
 require(MAIN, "void drawEditorStatic()")
